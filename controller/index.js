3b169a1e637941ad14476e9a89667a3edde693d5
RafiulAwal <rafiul.awal.66@gmail.com>
2021-08-21 14:21:27 +0600

    update

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const controller = {};
 
 let img; // for storing current uploaded image
@@ -9,7 +11,18 @@ controller.getImg = (req, res, next) => {
 };
 
 controller.postImg = (req, res, next) => {
-  console.log(req.file.filename);
+  // for removing old cropper image from file system.
+  if (img) {
+    fs.unlink(
+      path.join(
+        __dirname,
+        `../public/uploads/${req.file.destination.split("/").pop()}/${img}`
+      ),
+      (err) => console.log(err)
+    ); // req.file.destination.split("/").pop() for get the dir of uploaded image, which was defined with uploader middleware.
+  }
+
+  // store new image name for display in frontEnd
   img = req.file.filename;
   return res.redirect("/");
 };
